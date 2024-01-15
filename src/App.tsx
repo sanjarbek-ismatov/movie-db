380bafac716a3cd80514ceb827fa31b6658ddd6a
Sanjarbek <ismatovsanjarbek@yandex.ru>
2024-01-15 17:41:39 +0500

    some changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import Ui from "./components/ui";
 import "./styles/main.scss";
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { Props } from "./components/ui";
 function App() {
   const [array, setArray] = useState<Props | any>();
-  const [text, setText] = useState<string>("Terminator");
+  const [text, setText] = useState<string>("Creed");
   useEffect(() => {
     fetcher();
   }, []);
   const fetcher = async () => {
     await fetch(
-      `https://api.themoviedb.org/3/search/movie?api_key=f5eb67dea1a91e8974405abc90a7ae23&language=en-US&query=${text}&page=1&include_adult=true`
+      `https://api.themoviedb.org/3/search/movie?api_key=f5eb67dea1a91e8974405abc90a7ae23&language=en-US&query=${text}&page=1&include_adult=false`
     )
       .then((res) => res.json())
       .then((data) => {
